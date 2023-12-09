196749e7c0d4534f10e8a7dcf2a2df64b3c56484
JustinBenito <justinbenito1974@gmail.com>
2023-12-09 19:15:34 +0530

    landing page

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -24,12 +24,7 @@ const Hero = () => {
       initial="hidden"
       animate="visible"
       variants={textVariants}
-<<<<<<< HEAD
-      className='min-h-screen flex flex-col items-center justify-center relative bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-200 via-fuchsia-600 to-orange-600'
-      style={{ zIndex: 2 }} // Set a higher z-index for the Hero component
-=======
       className="min-h-screen flex flex-col items-center justify-center relative bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-200 via-fuchsia-600 to-orange-600"
->>>>>>> 300279d36b5dd1d2a89b08df3ca2de4622f6e88b
     >
       <div className="absolute inset-0 bg-noise"></div>
       <motion.h1
@@ -48,16 +43,8 @@ const Hero = () => {
         Your personalized space on the web. Showcase everything in one place.
       </motion.p>
       <motion.button
-<<<<<<< HEAD
-       variants={wordVariants}
-       className='mt-8 bg-white text-fuchsia-600 py-2 z-100 px-4 rounded-full font-bold'
-       whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
-       whileTap={{ scale: 0.95 }}
-       style={{ cursor: 'pointer' }}
-=======
         variants={wordVariants}
         className="mt-8 bg-white text-fuchsia-600  py-2 px-4 rounded-full font-bold"
->>>>>>> 300279d36b5dd1d2a89b08df3ca2de4622f6e88b
       >
         Get Started
       </motion.button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Create from './components/landing/create'
 import Hero from './components/landing/hero'
@@ -14,19 +13,6 @@ const Main = () => {
     <Create />
     <Search />
     <Footer />
-=======
-import React from 'react';
-import Create from './components/landing/create';
-import Hero from './components/landing/hero';
-import Nav from './components/landing/nav';
-
-const Main = () => {
-  return (
-    <div className="min-h-screen flex flex-col bg-[#f2e8cf]">
-      <Nav />
-      <Hero />
-      <Create />
->>>>>>> 300279d36b5dd1d2a89b08df3ca2de4622f6e88b
     </div>
   );
 };
