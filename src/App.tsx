76d2cc772894c47d574af0da409e5d93a6eaedcd
JustinBenito <justinbenito1974@gmail.com>
2023-12-09 11:30:24 +0530

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,46 @@ import Preview from './components/Preview';
 import Profile from './components/Profile';
 import Socials from './components/Socials';
 import ExternalLinks from './components/ExternalLinks';
+import { Routes, Route } from 'react-router-dom';
+import Main from './main';
+import Layout from './components/layout/Layout';
 
-function App() {
-  return (
-    <div className="App w-full md:flex bg-gray-100">
-      <div className="md:w-full md:h-[88vh] py-10 overflow-y-scroll">
-        <Profile />
-        <div className="divider"></div>
-        <Socials />
-        <div className="divider"></div>
-        <ExternalLinks />
-        <div className="flex justify-center md:justify-end px-5">
-          <button className="mt-8 border-2 btn btn-neutral rounded-lg block w-2/3 py-2">
-            Add More Link
-          </button>
-        </div>
-      </div>
-      <div className="md:w-1/2 p-10 bg-white flex items-center justify-center">
-        <Preview />
+// Create a Main component
+const All = () => (
+  <div className="App w-full md:flex bg-gray-100">
+    <div className="md:w-full md:h-[88vh] py-10 overflow-y-scroll">
+      <Profile />
+      <div className="divider"></div>
+      <Socials />
+      <div className="divider"></div>
+      <ExternalLinks />
+      <div className="flex justify-center md:justify-end px-5">
+        <button className="mt-8 border-2 btn btn-neutral rounded-lg block w-2/3 py-2">
+          Add More Link
+        </button>
       </div>
     </div>
+    <div className="md:w-1/2 p-10 bg-white flex items-center justify-center">
+      <Preview />
+    </div>
+  </div>
+);
+
+// Modify App component to use Routes and define routes
+function App() {
+  return (
+    <Routes>
+      {/* Route for the current content */}
+      <Route path="/" element={<Main />} />
+      {/* Route for the Main component */}
+
+      <Route path="/app" element={
+      <Layout >
+      <All />
+      </Layout>
+      } />
+
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import Layout from './components/layout/Layout';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Mumbai, Base } from "@thirdweb-dev/chains";
+import { BrowserRouter } from "react-router-dom"; 
+import App from './App';
+import Main from './main';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+<BrowserRouter>
     <ThirdwebProvider activeChain={Mumbai} supportedChains={[Mumbai, Base]} clientId="4c0c97b7299c42caf2cb72b200e924d8">
-      <Layout>
         <App />
-      </Layout>
     </ThirdwebProvider>
-  </React.StrictMode>
+</BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
